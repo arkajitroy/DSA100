bbf840678c64e4fa8b7a232f176f052d7860f348
arkajitroy <arkajitroy11.official@gmail.com>
2023-09-07 08:37:06 +0530

    added: rotate image / rotate matrix

---
+++ Arrays/Rotate_image.java
@@ -0,0 +1,58 @@
+class Solution {
+
+    public static void rotate(int matrix[][]){
+        int n=matrix.length;
+
+        // transpose of the matrix
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            reverse(matrix[i]);
+        }
+    }
+
+    private static void reverse(int row[]){
+        int left=0, right= row.length-1;
+
+        while(left < right){
+            int temp = row[left];
+            row[left] = row[right];
+            row[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+     private static void printMatrix(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        rotate(matrix);
+
+        System.out.println("Rotated Matrix:");
+        printMatrix(matrix);
+    }
+}
